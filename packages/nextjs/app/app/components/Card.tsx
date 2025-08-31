f9ff5f78e8040c4a142d5b3e6a2339a300a970fe
Steven <stevenmolina2205@gmail.com>
2025-08-31 10:04:26 -0300

    fixes eslint errors 1

--- packages/nextjs/app/app/components/Card.tsx
+++ packages/nextjs/app/app/components/Card.tsx
@@ -4,16 +4,7 @@ import { useState } from "react";
 import CardRendering from "./CardRendering";
 
 export default function Card() {
+  const [isNextOne] = useState(true);
 
-    const [isNextOne, setIsNextOne] = useState(true);
-
-    const handleNextOne = () => {
-        setIsNextOne(true);
-    }
-    
-    return (
-        
-   <CardRendering isNextOne={isNextOne} />
-        
-    )
+  return <CardRendering isNextOne={isNextOne} />;
 }
